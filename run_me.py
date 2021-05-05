f22e775b70aab87466777959851d8657b6bdd2ff
Renda <renda.yigit@gmail.com>
2021-05-05 19:48:34 +0300

    Adding the scirpt and vscode configs

---
+++ run_me.py
@@ -0,0 +1,51 @@
+from pyrender import Mesh, Scene, Viewer, Node, PerspectiveCamera, OrthographicCamera, DirectionalLight
+from io import BytesIO
+import numpy as np
+import trimesh
+import requests
+import time
+import math
+
+scene = Scene(bg_color=[0,0,0])
+
+mesh_source = "https://github.com/zekihub/3dmodels/raw/master/models/3d/bin/heart.glb"
+mesh = trimesh.load(BytesIO(requests.get(mesh_source).content), file_type='glb')
+meshInstance = Mesh.from_trimesh(list(mesh.geometry.values())[0])
+meshNode = Node(mesh=meshInstance, matrix=np.eye(4))
+scene.add_node(meshNode)
+
+# lightSource = DirectionalLight(color=[1.0, 1.0, 1.0], intensity=20.0)
+# lightNode = Node(light=lightSource, matrix=np.eye(4))
+# scene.add_node(lightNode)
+
+camera = OrthographicCamera(xmag=1.0, ymag=1.0)
+cameraNode = Node(camera=camera, matrix=np.eye(4))
+scene.add_node(cameraNode)
+
+zoom = 2.5
+x = 0.0
+y = 0.3
+cam_pose = np.array([
+    [1.0, 0.0, 0.0, x],
+    [0.0, 1.0, 0.0, y],
+    [0.0, 0.0, 1.0, zoom],
+    [0.0, 0.0, 0.0, 1.0]
+])
+scene.set_pose(cameraNode, pose=cam_pose)
+
+viewer = Viewer(scene,
+run_in_thread=True, 
+record = True,
+rotate = True, 
+use_raymond_lighting = True, 
+use_direct_lighting = True, 
+rotate_rate = math.pi / 2.0,
+refresh_rate = 25,
+lighting_intensity = 5.0, 
+rotate_axis = [0,1,0])
+
+time.sleep(4.9)
+
+viewer.close_external()
+
+viewer.save_gif("out.gif")
